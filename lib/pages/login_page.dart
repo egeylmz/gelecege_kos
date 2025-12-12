168c000992bcf42a1d586c891b4dc092d7d1cc94
İdil Gürsoy <64634611+idilsg@users.noreply.github.com>
2025-12-13 01:13:51 +0300

    login page interface update

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F5F7),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
 
-// şimdilik boş kalsın.
-// logini main.darta yazmıştık.
-// daha sonra anonymous login eklerken hepsini buraya taşırız.
+            children: [
+              // logo olacaksa buraya
+              
+              // const SizedBox(height: 24), 
+              // logo koyduysak bunu commentten çıkar
+          
+              const Text("Welcome!",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 5),
+
+              Text("Please log in to continue",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[600],
+              ),
+              textAlign: TextAlign.center
+              ),
+
+              const SizedBox(height: 50),
+
+              // email input
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withValues(alpha: 0.5),
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    )
+                  ]
+                ),
+
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SvgPicture.asset(
+                        'assets/icons/usericon.svg',
+                        colorFilter: const ColorFilter.mode(
+                          Colors.black, 
+                          BlendMode.srcIn
+                          )
+                      )
+                    ),
+                    hintText: 'E-mail',
+                    hintStyle: TextStyle(color: Colors.grey[600]),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 15
+                    )
+                  ),
+                )
+              ),
+
+              const SizedBox(height: 16),
+
+              // password input
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withValues(alpha: 0.5),
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    )
+                  ]
+                ),
+
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SvgPicture.asset(
+                        'assets/icons/passwordicon.svg',
+                        colorFilter: const ColorFilter.mode(
+                          Colors.black,
+                          BlendMode.srcIn,
+                        )
+                      ),
+                    ),
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color:Colors.grey[600]),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 15.0,
+                    )
+                  ),
+                )
+              ),
+
+              const SizedBox(height: 32),
+
+            ]
+          ),
+        )
+      )
+    );
+  }
+}
